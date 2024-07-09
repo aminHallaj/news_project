21e3119a442ce2197fb2d07021ea9ade27654dfc
Amin-Hallaj <aminhallajm@gmail.com>
2024-07-09 16:03:45 +0330

    ایجاد صفحه ورود و صفحه داشبورد

--- news_site/master/urls.py
+++ news_site/master/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('master/dashboard/', views.master_panel, name='master_panel'),
+    path('master/signin/', views.master_signin, name='master_signin'),
+    path('master/signin/submit/', views.master_signin_submit, name='master_signin_submit'),
+    path('master/logout/', views.master_logout, name='master_logout'),
 
 ]
--- news_site/master/views.py
+++ news_site/master/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 , redirect
+from django.contrib.auth import login , authenticate , logout 
+from django.contrib.auth.models import  User , Permission
+from django.contrib import messages
+from settings_site.models import *
 
-# Create your views here.
+
+
+def master_panel(request):
+
+    settings = Settings.objects.get(id=1)
+
+    
+    list_dashboard = {
+        "settings":settings,
+    }
+
+    return render(request, 'master/dashboard.html', list_dashboard)
+
+
+def master_signin(request):
+
+    settings = Settings.objects.get(id=1)
+            
+
+    list_signin = {
+        "settings":settings,
+    }
+
+    return render(request, 'master/signin.html', list_signin)
+
+
+def master_signin_submit(request):
+    
+    if request.method == "POST":
+
+        usernams = request.POST.get('username')
+        passwords = request.POST.get('password')
+        user = authenticate(username = usernams , password = passwords)
+
+        if user is not None:
+
+            login(request,user)
+            messages.success(request, f"  {request.user.get_full_name()} خوش اومدین.")
+            return redirect("master_panel")
+            
+        else:
+            messages.error(request,"نام کاربری یا رمز عبور نامعتبر است.")
+            return redirect("master_signin")
+            
+    return redirect("master_signin")
+
+
+def master_logout(request):
+
+    logout(request)
+    return redirect("master_signin")
